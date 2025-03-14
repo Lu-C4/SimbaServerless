f428f717dc1706ae8b702f9b1037686c69d40538
Lu-C4 <153966107+Lu-C4@users.noreply.github.com>
2025-03-14 12:50:19 +0530

    add clans ranking

--- commands.py
+++ commands.py
@@ -213,6 +213,40 @@ class GetClanRanking(SlashCommand):
 
         await send_followup(interaction_token, "*Roars*", [stats_embed])        
 
+class GetClanRank(SlashCommand):
+    def __init__(self):
+        super().__init__(
+            name="clanrank",
+            description="Display the weekly clans ranking.",
+        )
+
+
+
+    async def respond(self, json_data: dict):
+        interaction_token = json_data["token"]
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"https://ev.io/clans")
+        tree = html.fromstring(response.content)
+        matches = tree.xpath('//td[contains(concat( " ", @class, " " ), concat( " ", "is-active", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "img-responsive", " " ))] | //td//a | //td[contains(concat( " ", @class, " " ), concat( " ", "views-field-counter", " " ))]')
+
+        scores = []
+
+
+        for i in range(0, len(matches),4):
+            clan={}
+            clan["name"]=matches[i].text_content().strip()+"."+matches[i+2].text_content().strip()
+            clan["value"]=matches[i+3].text_content().strip()
+            clan["image"]="ev.io"+matches[i+1].attrib['src']
+            clan["inline"]=False
+            scores.append(clan)
+
+        stats_embed = {
+            "color": 16776960,  # Yellow
+            "fields": scores[:10],  
+        }
+
+        await send_followup(interaction_token, "", [stats_embed])        
+
 class CheckSurvivalScores(SlashCommand):
     def __init__(self):
         super().__init__(
@@ -367,7 +401,7 @@ class PeekSkins(SlashCommand):
         payload = {"content": message, "embeds": embeds}
         headers = {"Content-Type": "application/json"}
         requests.post(url, json=payload, headers=headers)    
-commands = [CheckPlayerStats(),CheckSurvivalScores(),GetCrosshair(),PeekSkins(),GetClanRanking()]
+commands = [CheckPlayerStats(),CheckSurvivalScores(),GetCrosshair(),PeekSkins(),GetClanRanking(),GetClanRank()]
 
 
 @app.post("/")
